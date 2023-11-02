1001cf7ef1735e4a06f50d544980732698d3cff6
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-02 15:27:03 +0900

    feat: 모든 글 조회하는 api

--- router/post.js
+++ router/post.js
@@ -2,9 +2,25 @@ const express = require('express');
 const router = express.Router();
 const cors = require('cors');
 const db = require('../util/db'); 
+const { default: axios } = require('axios');
 
 router.use(express.json()); 
 router.use(express.urlencoded({ extended: false })); // URL 인코딩 미들웨어
 router.use(cors());
+
+router.get('/', (req, res) => {
+    db.query(
+        'SELECT * FROM post', 
+        (selectPostError, posts) => {
+        if (selectPostError) {
+            res.json(selectPostError);
+        } else {
+            res.json(posts);
+        }
+    });
+});
+
+
+
 
 module.exports = router;
