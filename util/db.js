9b609f8fc8b21a819c045bfe4d2d2d597d741a28
hyeg0121 <w2218@e-mirim.hs.kr>
2023-11-18 15:50:32 +0900

    feat: db 정보 env 파일에서 가져옴

--- util/db.js
+++ util/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '1234567890',
-  database: 'gogit'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PW,
+  database: process.env.DB_NAME
 });
 
 module.exports = db;
